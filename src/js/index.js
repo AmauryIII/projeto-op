ec86e993a3be34b8226959735bf07bbb89aa155d
Amaury Lopes Hissa Terceiro <123034113+AmauryIII@users.noreply.github.com>
2023-10-04 11:09:47 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+// OBJETIVO 1 - quando clicar no botao do personagem na lista, marcar o botão como selecionado
+
+const botoes = document.querySelectorAll(".botao");
+const personagens = document.querySelectorAll(".personagem");
+
+botoes.forEach((botao, indice) => {
+  botao.addEventListener("click", () => {
+    desselecionarBotao();
+
+    botao.classList.add("selecionado");
+
+    const personagemSelecionado = document.querySelector(
+      ".personagem.selecionado"
+    );
+    personagemSelecionado.classList.remove("selecionado");
+
+    personagens[indice].classList.add("selecionado");
+  });
+});
+function desselecionarBotao() {
+
+  desselecionarPersonagem();
+  
+}
+function desselecionarPersonagem() {
+  const botaoSelecionado = document.querySelector(".botao.selecionado");
+  botaoSelecionado.classList.remove("selecionado");
+}
